224301ed7f3f2eeb2ec8c67ca34a612bbc874b9a
Alina To <alina.to@gmail.com>
2019-09-05 10:25:32 -0700

    Initiate app; add new files;

--- agenda-app/start-here/src/app/app.module.ts
+++ agenda-app/start-here/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { EventComponent } from '../components/event/event.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
